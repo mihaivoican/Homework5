be8412c48d987f134e3b96e9cf84435d504d02f1
ddcmpower <ddcmpower@gmail.com>
2022-08-24 15:08:07 +0300

    al 2lea commit

--- src/Homework5Exercises.java
+++ src/Homework5Exercises.java
@@ -2,7 +2,10 @@ public class Homework5Exercises {
     static int[] sirIntregi = {12,33,10,100};
 
     public static void main(String[] args) {
+        System.out.println("Ex 1");
         System.out.println(sumaNumere(sirIntregi));
+        System.out.println("Ex 2");
+        System.out.println(cautNrImpareInArray(sirIntregi));
     }
 
     //1.  scrieti o functie care primeste un sir de numere intregi si returneaza suma lor
@@ -12,5 +15,16 @@ public class Homework5Exercises {
             suma +=numar;
         }
         return suma;
+    }
+
+    //2 scrieti o functie care primeste un sir de numere intregi si returneaza numarul de elemente impare
+    public static int cautNrImpareInArray(int[] sirNr){
+        int nrImpare=0;
+        for(int nr :sirNr){
+            if (nr % 2 != 0){
+                nrImpare++;
+            }
+        }
+        return nrImpare;
     }
 }
