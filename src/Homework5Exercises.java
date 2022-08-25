684b1d01df284eb5ca52828438b777335aeda6b7
ddcmpower <ddcmpower@gmail.com>
2022-08-25 13:51:11 +0300

    cu 3 exercitii

--- src/Homework5Exercises.java
+++ src/Homework5Exercises.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Homework5Exercises {
     static int[] sirIntregi = {12,33,10,100};
 
@@ -6,6 +8,9 @@ public class Homework5Exercises {
         System.out.println(sumaNumere(sirIntregi));
         System.out.println("Ex 2");
         System.out.println(cautNrImpareInArray(sirIntregi));
+        System.out.println("Ex 3");
+        System.out.println(Arrays.toString(returnNoGreater(sirIntregi,30)));
+
     }
 
     //1.  scrieti o functie care primeste un sir de numere intregi si returneaza suma lor
@@ -29,15 +34,14 @@ public class Homework5Exercises {
     }
 
     //3- scrieti o functie care primeste un sir de numere intregi si un alt numar intreg. Returnati toate numerele mai mari decat numarul primit
-    public static int[] returnNoGreater(int[] sirIntregi, int nrDeTestat){
-        int[] resultArray =new int[sirIntregi.length];
-        int size=0;
+    public static String[] returnNoGreater(int[] sirIntregi, int nrDeTestat){
+        String resultSir ="";
         for (int i=0; i<sirIntregi.length; i++){
-            if(sirIntregi[i] < nrDeTestat){
-                resultArray[i]=sirIntregi[i];
+            if(sirIntregi[i] > nrDeTestat){
+                resultSir = resultSir + sirIntregi[i] + ",";
             }
         }
-        //vezi ultim site pt redim
-        resultArray.srinkSize();
+        String[] resultArray = resultSir.split(",");
+        return resultArray;
     }
 }
