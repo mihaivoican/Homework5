29d8ad374d9bf9a91cf5169263e1315f3919a102
ddcmpower <ddcmpower@gmail.com>
2022-08-26 09:44:34 +0300

    6 exercitii

--- src/Homework5Exercises.java
+++ src/Homework5Exercises.java
@@ -14,7 +14,9 @@ public class Homework5Exercises {
         System.out.println("Ex 4");
         System.out.println(adaugDonatii(100000));
         System.out.println("Ex 5");
-        System.out.println(adaugDonatii2(1000000,2));
+        System.out.println(adaugDonatii2(1000000, 2));
+        System.out.println("Ex 6");
+        spargeFraza("Aseara am fost la pasure. A fost frumos pana la un punct. Apoi a venit ploaia. Am fugit repede la masini. Asta a fost escapada de duminica.");
     }
 
     //1.  scrieti o functie care primeste un sir de numere intregi si returneaza suma lor
@@ -49,7 +51,7 @@ public class Homework5Exercises {
         return resultArray;
     }
 
- //4) scrieti o functie care primeste un numar intreg reprezentand targetul de donatii. Donatiile se vor face cu ajutorul obiectului Random. Primim donatii pana cand ajungem la suma dorita. Cand ajungem la suma dorita afisam un mesaj de succes.
+    //4) scrieti o functie care primeste un numar intreg reprezentand targetul de donatii. Donatiile se vor face cu ajutorul obiectului Random. Primim donatii pana cand ajungem la suma dorita. Cand ajungem la suma dorita afisam un mesaj de succes.
     public static String adaugDonatii(int valMax) {
         int total = 0;
         Random rand = new Random();
@@ -59,7 +61,7 @@ public class Homework5Exercises {
         return "S-a atins suma dorita. Multumim pt donatii";
     }
 
- //5) rescrieti functia de la 4 cu urmatoarea modificare: functia va primi si un numar maxim de donatii. Cand acesta se termina, campania se va incheia
+    //5) rescrieti functia de la 4 cu urmatoarea modificare: functia va primi si un numar maxim de donatii. Cand acesta se termina, campania se va incheia
     public static String adaugDonatii2(int valMax, int nrDonatii) {
         int total = 0;
         int nrDonatiiCrt = 0;
@@ -67,13 +69,21 @@ public class Homework5Exercises {
         do {
             total += rand.nextInt(valMax);
             nrDonatiiCrt++;
-        } while (total <= valMax && nrDonatiiCrt < nrDonatii) ;
-        if(total >= valMax){
-        return "S-a atins suma dorita. Multumim pt donatii";
-    } else if (nrDonatiiCrt == nrDonatii) {
+        } while (total <= valMax && nrDonatiiCrt < nrDonatii);
+        if (total >= valMax) {
+            return "S-a atins suma dorita. Multumim pt donatii";
+        } else if (nrDonatiiCrt == nrDonatii) {
             return "Multumim pt donatii. Campania s-a incheiat";
-        }else {
+        } else {
             return "Ceva ciudat s-a intamplat!";
         }
     }
+
+    //6  scrieti o functie care primeste un string cu o fraza (mai multe propozitii despartite prin punct). Printati fiecare propozitie pe o linie noua
+    public static void spargeFraza(String fraza){
+        String[] frazaSparta = fraza.split("[.]");
+        for(String propozitie :frazaSparta){
+            System.out.println(propozitie.trim() +".");
+        }
     }
+}
